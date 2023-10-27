130dde6feaafba2dc64430b2fca35c57d6b787f2
MahmoudAlHaj4 <alhajmahmoud247@gmail.com>
2023-10-27 20:51:17 +0300

    Add Matrices function

--- assignment3.py
+++ assignment3.py
@@ -4,4 +4,9 @@ print("welcome", name)
 
 def displayMenu():
     print("The menu:\n","\t1.Add Matrices\n","\t2. Check Rotation\n","\t3. Invert Dictionary\n","\t4. Convert Matrix to Dictionary\n","\t5. Check Palindrome\n","\t6. Search for an Element & Merge Sort\n","\t7. Exit\n")
-    
+
+
+def addMatrices():
+   num_row = int(input("Enter the number of row: ")) 
+   num_col = int(input("Enter the number of col: "))
+   # inputs to get the number of rows and cols for the Matrix
