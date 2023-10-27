0fe7d0fce0956ceee5b58a3f06d85c161fdd912d
MahmoudAlHaj4 <alhajmahmoud247@gmail.com>
2023-10-27 20:46:39 +0300

    display menu function

--- assignment3.py
+++ assignment3.py
@@ -1,3 +1,7 @@
 name = str(input("Enter your name: "))
 print("welcome", name)
 # print welcome mssg
+
+def displayMenu():
+    print("The menu:\n","\t1.Add Matrices\n","\t2. Check Rotation\n","\t3. Invert Dictionary\n","\t4. Convert Matrix to Dictionary\n","\t5. Check Palindrome\n","\t6. Search for an Element & Merge Sort\n","\t7. Exit\n")
+    
