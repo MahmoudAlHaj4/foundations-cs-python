4623c5056024c83f5cf1e2117bf226668b543278
MahmoudAlHaj4 <alhajmahmoud247@gmail.com>
2023-11-09 19:10:36 +0200

    add Menu Function

--- midterm.py
+++ midterm.py
@@ -1,1 +1,3 @@
-# 
+# add menu 
+def displayMenu():
+    print("The Menu:\n", "\t1. Open Tab\n","\t2. Close Tab\n","\t3. Switch Tab\n","\t4. Display All Tabs\n","\t5. Open Nested Tab\n","\t6. Clear All Tabs\n","\t7. Save Tabs\n","\t8. Import Tabs\n","\t9. Exit\n")
