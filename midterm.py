bdcd50f47f98d98e1719ab1d87c734afae2d68fc
MahmoudAlHaj4 <alhajmahmoud247@gmail.com>
2023-11-09 19:23:52 +0200

    fix main function

--- midterm.py
+++ midterm.py
@@ -4,4 +4,27 @@ def displayMenu():
 
 def main():
     displayMenu()
+
+    choice = 0 
+    while choice != 9:
+        choice = int(input("Enter Your Choice: "))
+        if choice == 1:
+            print("open tab")
+        elif choice == 2:
+            print("close Tab")
+        elif choice  == 3:
+            print("Switch Tab")
+        elif choice  == 4:
+            print("Display All Tabs")
+        elif choice == 5:
+            print("Open Nested Tab")
+        elif choice == 6:
+            print("Clear All Tabs")
+        elif choice == 7:
+            print("Save Tabs")
+        elif choice == 8:
+            print("Import Tabs")
+        elif choice != 9:
+            print("Invalid Input Please try again")
+    print("You left")
 main()
