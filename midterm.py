1d61a142b94a0a7d1fc1bdbcd780dd40c54d2968
MahmoudAlHaj4 <alhajmahmoud247@gmail.com>
2023-11-10 10:57:54 +0200

    update closeTab function , add class, move dict

--- midterm.py
+++ midterm.py
@@ -1,36 +1,44 @@
-# add menu 
-def displayMenu():
-    print("The Menu:\n", "\t1. Open Tab\n","\t2. Close Tab\n","\t3. Switch Tab\n","\t4. Display All Tabs\n","\t5. Open Nested Tab\n","\t6. Clear All Tabs\n","\t7. Save Tabs\n","\t8. Import Tabs\n","\t9. Exit\n")
+class BrowserTabsSimulation:
+    def __init__(self):
+        self.dict_for_tabs = {}
 
-def openTab():
-    dict_for_tabs = {}
+    def displayMenu(self):
+        print("The Menu:\n", "\t1. Open Tab\n","\t2. Close Tab\n","\t3. Switch Tab\n","\t4. Display All Tabs\n","\t5. Open Nested Tab\n","\t6. Clear All Tabs\n","\t7. Save Tabs\n","\t8. Import Tabs\n","\t9. Exit\n")
+
+    def openTab(self):
+        
+        num_of_tabs = int(input("Enter the number of tabs: "))
+        for x in range(1,num_of_tabs+1):
+            new_dict ={}
+            for y in range(1):
+                title = input("Enter the website title: ")
+                url = input("Enter the website url:  ")
+                new_dict['title'] = title
+                new_dict['url'] = url
+            id = "ID" + str(x)
+            self.dict_for_tabs[id] = new_dict
+            print(self.dict_for_tabs)
+
+    def closeTab(self):
+        index = input("Enter index of the tab you want to close: ")
+        if index in self.dict_for_tabs:
+            del self.dict_for_tabs[index]
+        else:
+            print("Not found")
     
-    num_of_tabs = int(input("Enter the number of tabs: "))
-    for x in range(1,num_of_tabs+1):
-        new_dict ={}
-        for y in range(1):
-            title = input("Enter the website title: ")
-            url = input("Enter the website url:  ")
-            new_dict['title'] = title
-            new_dict['url'] = url
-        id = "ID" + str(x)
-        dict_for_tabs[id] = new_dict
-        print(dict_for_tabs)
-
-def closeTab():
-    index = input("Enter index of the tab you want to close: ")
         
 
 def main():
-    displayMenu()
+    browser = BrowserTabsSimulation()
+    browser.displayMenu()
 
     choice = 0 
     while choice != 9:
         choice = int(input("Enter Your Choice: "))
         if choice == 1:
-            openTab()
+            browser.openTab()
         elif choice == 2:
-            print("close Tab")
+            browser.closeTab()
         elif choice  == 3:
             print("Switch Tab")
         elif choice  == 4:
