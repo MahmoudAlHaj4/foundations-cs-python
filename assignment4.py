553d5433b982f5d7d80be08a04b1d2116ad4a1d9
MahmoudAlHaj4/ <alhajmahmoud247@gmail.com>
2023-11-19 14:55:18 +0200

    add name ,menu function and main

---
+++ assignment4.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ").capitalize()
+print("Welcome", name)
+
+def displayMenu():
+    print("The menu:\n","\t1. Singly Linked List\n","\t2. Check if Palindrome\n","\t3. Priority Queue\n","\t4. Evaluate an Infix Expression\n","\t5. Graph\n","\t6. Exit\n")
+
+
+
+
+def main():
+    print()
+    displayMenu()
+    print()
+main()
