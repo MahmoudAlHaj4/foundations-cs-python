f5f44d0903a7f8fd7f95f9b061cc88d7153304c6
MahmoudAlHaj4/ <alhajmahmoud247@gmail.com>
2023-11-19 15:00:49 +0200

    add Node, linkedList, and documentation

--- assignment4.py
+++ assignment4.py
@@ -1,11 +1,17 @@
 name = input("Enter your name: ").capitalize()
 print("Welcome", name)
 
-def displayMenu():
+def displayMenu(): # function to display Menu for user
     print("The menu:\n","\t1. Singly Linked List\n","\t2. Check if Palindrome\n","\t3. Priority Queue\n","\t4. Evaluate an Infix Expression\n","\t5. Graph\n","\t6. Exit\n")
 
-
-
+class Node: #Class Node 
+    def __init__(self,info):
+        self.head = None
+        self.info = info
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
 def main():
     print()
